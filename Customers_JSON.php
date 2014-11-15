b7493c736678accfb1cfa83b5255ec114f638011
crysisx <843984246@qq.com>
2014-11-15 23:41:37 +0800

    Revert "Revert "Revert "Revert "Angular""""
    This reverts commit edc5a49b9e45ddc81b1860184f264fdfda89c0c8.

---
+++ Customers_JSON.php
@@ -0,0 +1,87 @@
+[
+{
+"Name" : "Alfreds Futterkiste",
+"City" : "Berlin",
+"Country" : "Germany"
+},
+{
+"Name" : "Berglunds snabbk?p",
+"City" : "Lule?",
+"Country" : "Sweden"
+},
+{
+"Name" : "Centro comercial Moctezuma",
+"City" : "M¨¦xico D.F.",
+"Country" : "Mexico"
+},
+{
+"Name" : "Ernst Handel",
+"City" : "Graz",
+"Country" : "Austria"
+},
+{
+"Name" : "FISSA Fabrica Inter. Salchichas S.A.",
+"City" : "Madrid",
+"Country" : "Spain"
+},
+{
+"Name" : "Galer¨ªa del gastr¨®nomo",
+"City" : "Barcelona",
+"Country" : "Spain"
+},
+{
+"Name" : "Island Trading",
+"City" : "Cowes",
+"Country" : "UK"
+},
+{
+"Name" : "K?niglich Essen",
+"City" : "Brandenburg",
+"Country" : "Germany"
+},
+{
+"Name" : "Laughing Bacchus Wine Cellars",
+"City" : "Vancouver",
+"Country" : "Canada"
+},
+{
+"Name" : "Magazzini Alimentari Riuniti",
+"City" : "Bergamo",
+"Country" : "Italy"
+},
+{
+"Name" : "North/South",
+"City" : "London",
+"Country" : "UK"
+},
+{
+"Name" : "Paris sp¨¦cialit¨¦s",
+"City" : "Paris",
+"Country" : "France"
+},
+{
+"Name" : "Rattlesnake Canyon Grocery",
+"City" : "Albuquerque",
+"Country" : "USA"
+},
+{
+"Name" : "Simons bistro",
+"City" : "K?benhavn",
+"Country" : "Denmark"
+},
+{
+"Name" : "The Big Cheese",
+"City" : "Portland",
+"Country" : "USA"
+},
+{
+"Name" : "Vaffeljernet",
+"City" : "?rhus",
+"Country" : "Denmark"
+},
+{
+"Name" : "Wolski Zajazd",
+"City" : "Warszawa",
+"Country" : "Poland"
+}
+]
